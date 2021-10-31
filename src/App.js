b3c7b9e206ed6ee1168f67c7fff136a97c249c21
Moni Shankar <32106437+Moni05@users.noreply.github.com>
2021-10-31 23:07:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import PortfolioContainer from "./component/PortfolioContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <PortfolioContainer />
+    </div>
+  );
+}
+
+export default App;
